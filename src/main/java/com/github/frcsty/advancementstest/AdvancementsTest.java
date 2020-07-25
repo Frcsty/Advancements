463e335eb3ee7919156134502e86207d0f6c6a03
Frosty <frostyprime13@gmail.com>
2020-07-25 15:47:58 +0200

    Finished - Toast Display Lasts ~130 ticks

--- src/main/java/com/github/frcsty/advancementstest/AdvancementsTest.java
+++ src/main/java/com/github/frcsty/advancementstest/AdvancementsTest.java
@@ -2,69 +2,31 @@ package com.github.frcsty.advancementstest;
 
 import com.github.frcsty.advancementapi.AdvancementAPI;
 import com.github.frcsty.advancementapi.manager.AdvancementManager;
-import com.github.frcsty.advancementapi.wrapper.Advancement;
-import com.github.frcsty.advancementapi.wrapper.AdvancementDisplay;
-import com.github.frcsty.advancementapi.wrapper.AdvancementVisibility;
-import com.github.frcsty.advancementapi.wrapper.NameKey;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
+import com.github.frcsty.advancementstest.listener.BlockBreakListener;
+import com.github.frcsty.advancementstest.listener.PlayerJoinListener;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 public final class AdvancementsTest extends JavaPlugin implements Listener {
 
-    private final AdvancementManager manager = AdvancementAPI.getNewAdvancementManager();
+    private static final AdvancementManager MANAGER = AdvancementAPI.getNewAdvancementManager();
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
-    }
-
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                manager.addPlayer(event.getPlayer());
-            }
-        }.runTaskLater(this, 2L);
-    }
-
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        final ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
-        if (item.getType() == Material.AIR) return;
-        if (!item.getType().toString().contains("PICKAXE")) return;
-
-        handleAdvancement(item, event.getPlayer());
-    }
-
-    private void handleAdvancement(final ItemStack item, final Player player) {
-        final Damageable meta = (Damageable) item.getItemMeta();
-        if (meta == null) return;
-
-        final int durability = (item.getType().getMaxDurability() - meta.getDamage()) - 1;
-        if (durability > 25) return;
-        final String title = durability == 1 ? "§cItem Break Alert!" : "§cLow Durability Alert! (" + durability + ")";
-        final String description = "Durability Indication Advancement";
-
-        final AdvancementDisplay display = new AdvancementDisplay(
-                item.getType(),
-                title,
-                description,
-                AdvancementDisplay.AdvancementFrame.CHALLENGE,
-                true,
-                false,
-                AdvancementVisibility.VANILLA
+        registerListeners(
+                new PlayerJoinListener(this),
+                new BlockBreakListener(this)
         );
-        final Advancement advancement = new Advancement(null, new NameKey("custom", "damage"), display);
+    }
 
-        advancement.displayToast(player);
+    private void registerListeners(@NotNull final Listener... listeners) {
+        Arrays.asList(listeners).forEach(listener -> getServer().getPluginManager().registerEvents(listener, this));
+    }
+
+    public AdvancementManager getManager() {
+        return MANAGER;
     }
 }
---
+++ src/main/java/com/github/frcsty/advancementstest/listener/BlockBreakListener.java
@@ -0,0 +1,86 @@
+package com.github.frcsty.advancementstest.listener;
+
+import com.github.frcsty.advancementapi.wrapper.Advancement;
+import com.github.frcsty.advancementapi.wrapper.AdvancementDisplay;
+import com.github.frcsty.advancementapi.wrapper.AdvancementVisibility;
+import com.github.frcsty.advancementapi.wrapper.NameKey;
+import com.github.frcsty.advancementstest.AdvancementsTest;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public final class BlockBreakListener implements Listener {
+
+    private static final List<Player> AVAILABILITY = new ArrayList<>();
+    private static final List<Material> ALLOWED = Arrays.asList(
+            Material.WOODEN_PICKAXE, Material.STONE_PICKAXE, Material.IRON_PICKAXE, Material.GOLDEN_PICKAXE,
+            Material.DIAMOND_PICKAXE, Material.NETHERITE_PICKAXE);
+    private static final int TOAST_DURATION = 130;
+    @NotNull private final AdvancementsTest plugin;
+
+    public BlockBreakListener(@NotNull final AdvancementsTest plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        final Player player = event.getPlayer();
+        final ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
+        if (item.getType() == Material.AIR && !ALLOWED.contains(item.getType())) return;
+
+        final Damageable meta = (Damageable) item.getItemMeta();
+        if (meta == null) return;
+
+        final int durability = (item.getType().getMaxDurability() - meta.getDamage()) - 1;
+
+        if (AVAILABILITY.contains(player)) return;
+        handleAdvancement(item, durability, event.getPlayer());
+    }
+
+    private String getDisplayTitle(final int durability) {
+        if (durability <= 2) {
+            return "§cYour item is about to break!";
+        } else if (durability <= 5) {
+            return "§4Low item durability! §7- §4" + durability;
+        } else if (durability <= 60){
+            return "§cLow item durability! §7- §c" + durability;
+        } else {
+            return null;
+        }
+    }
+
+    private void handleAdvancement(final ItemStack item, final int durability, final Player player) {
+        final String displayTitle = getDisplayTitle(durability);
+        if (displayTitle == null) return;
+
+        final AdvancementDisplay display = new AdvancementDisplay(
+                item.getType(),
+                displayTitle,
+                "",
+                AdvancementDisplay.AdvancementFrame.CHALLENGE,
+                true,
+                false,
+                AdvancementVisibility.VANILLA
+        );
+        final Advancement advancement = new Advancement(null, new NameKey("custom", "damage"), display);
+
+        advancement.displayToast(player);
+        AVAILABILITY.add(player);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                AVAILABILITY.remove(player);
+            }
+        }.runTaskLater(plugin, TOAST_DURATION);
+    }
+}
